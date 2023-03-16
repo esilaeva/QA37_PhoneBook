489fcc5cfdd5b0389ed04fbae3a2a5292ba76311
esilaeva <emsilaeva@gmail.com>
2023-03-16 21:09:34 +0200

    lesson1603

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,14 +1,33 @@
 package tests;
 
+import manager.HelperUser;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
     @Test
-    public void test(){
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("pups+1@gmail.com","Ff12345$");
+        app.getHelperUser().submitLogin();
 
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("pups+1@gmail.com","Ff12345$");
+        app.getHelperUser().submitLogin();
 
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
 
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,7 +13,7 @@ public class TestBase {
     }
     @AfterSuite
     public void tearDown(){
-        app.stop();
+       // app.stop();
     }
 
 }
