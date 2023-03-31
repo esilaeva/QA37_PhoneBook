66265eceec5b2fc2883ddac8bcfd5749b92bf5d2
esilaeva <emsilaeva@gmail.com>
2023-03-31 16:49:42 +0300

    AddNewContactPositive

--- src/main/java/manager/HelperContact.java
+++ src/main/java/manager/HelperContact.java
@@ -1,9 +1,33 @@
 package manager;
 
+import models.Contact;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperContact extends HelperBase{
     public HelperContact(WebDriver wd) {
         super(wd);
+    }
+
+    public void openContactForm() {
+        click(By.xpath("//a[text()='ADD']"));
+    }
+
+    public void fillContactForm(Contact contact) {
+        type(By.xpath("//input[@placeholder='Name']"), contact.getName());
+        type(By.xpath("//input[@placeholder='Last Name']"), contact.getLastName());
+        type(By.xpath("//input[@placeholder='Phone']"), contact.getPhone());
+        type(By.xpath("//input[@placeholder='email']"), contact.getEmail());
+        type(By.xpath("//input[@placeholder='Address']"), contact.getAddress());
+        type(By.xpath("//input[@placeholder='description']"), contact.getDescription());
+    }
+
+
+    public void submitForm() {
+        click(By.xpath("//*[text()='Save']"));
+    }
+
+    public boolean isActiveContacts() {
+        return isElementPresent(By.xpath("//*[@class='active'][text()='CONTACTS']"));
     }
 }
--- src/test/java/tests/AddNewContactTests.java
+++ src/test/java/tests/AddNewContactTests.java
@@ -1,26 +1,55 @@
 package tests;
 
 import models.Contact;
+import models.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Random;
 
-public class AddNewContactTests extends TestBase{
-    @Test
-    public void addNewContact(){
-        int i = (int) System.currentTimeMillis()/1000;
-        Contact contact = Contact.builder()
-                .name("")
-                .lastName("")
-                .phone("")
-                .address("")
-                .description("")
-                .build();
-        app.getHelperContact().openContactForm();
-        app.getHelperContact().fillContactForm();
-        app.getHelperContact().submitForm();
+public class AddNewContactTests extends TestBase {
 
-
+    @BeforeClass
+    public void login() {
+        if (!app.getHelperUser().isLogged()) {
+            app.getHelperUser().login(new User().setEmail("b.snyder@gmail.com").setPassword("Tt12345$"));
+        }
     }
 
+    @Test
+    public void addNewContactAll() {
+        int i = (int) System.currentTimeMillis() / 1000;
+        Contact contact = Contact.builder()
+                .name("Vera")
+                .lastName("Salt")
+                .phone("972345" + i)
+                .email("v.salt" + i + "@gmail.com")
+                .address("Tel-Aviv")
+                .description("The best friend")
+                .build();
+        app.getHelperContact().openContactForm();
+        app.getHelperContact().fillContactForm(contact);
+        app.getHelperContact().submitForm();
+
+        Assert.assertTrue(app.getHelperContact().isActiveContacts());
+    }
+
+
+    @Test
+    public void addNewContact() {
+        int i = (int) System.currentTimeMillis() / 1000;
+        Contact contact = Contact.builder()
+                .name("Super")
+                .lastName("Girl")
+                .phone("972134" + i)
+                .email("s.girl" + i + "@gmail.com")
+                .address("Tel-Aviv")
+                .build();
+        app.getHelperContact().openContactForm();
+        app.getHelperContact().fillContactForm(contact);
+        app.getHelperContact().submitForm();
+
+        Assert.assertTrue(app.getHelperContact().isActiveContacts());
+    }
 }
